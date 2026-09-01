ecc094d6e4a71b3a31e75b59ce7597ec868ff1fa
temii-mp3 <Ogunleyeamiel@gmail.com>
2026-09-01 17:10:02 -0500

    refactor

---
+++ Library.Infrastructure/Exceptions.cs
@@ -0,0 +1,113 @@
+using System.Security.Cryptography.X509Certificates;
+using System.Xml.Xsl;
+
+[Serializable]
+public class AccountNotFoundException : Exception
+{
+    public AccountNotFoundException() : base("Account not found") { }
+    public AccountNotFoundException(string message) : base(message) { }
+};
+
+[Serializable]
+public class AccountExistsException : Exception
+{
+    public AccountExistsException() : base("Account already exists") { }
+    public AccountExistsException(string message) : base(message) { }
+}
+
+[Serializable]
+public class InvalidAccountException : Exception
+{
+    public InvalidAccountException() { }
+    public InvalidAccountException(string message) : base(message) { }
+};
+[Serializable]
+public class BookNotFoundException : Exception
+{
+    public BookNotFoundException() : base("Book not found") { }
+    public BookNotFoundException(string message) : base(message) { }
+};
+[Serializable]
+public class GenericException : Exception
+{
+    public GenericException() : base("Something went wrong") { }
+    public GenericException(string message) : base(message) { }
+};
+[Serializable]
+public class RoomLimitReachedException : Exception
+{
+    public RoomLimitReachedException() { }
+    public RoomLimitReachedException(string message) : base(message) { }
+}
+
+
+[Serializable]
+public class BookLimitReachedException : Exception
+{
+    public BookLimitReachedException() { }
+    public BookLimitReachedException(string message) : base(message) { }
+};
+[Serializable]
+public class NotLoggedInException : Exception
+{
+    public NotLoggedInException() : base("You need to Log in") { }
+    public NotLoggedInException(string message) : base(message) { }
+};
+[Serializable]
+public class InvalidInputException : Exception
+{
+    public InvalidInputException() { }
+    public InvalidInputException(string message) : base(message) { }
+};
+[Serializable]
+public class BookBorrowedException : Exception
+{
+    public BookBorrowedException() { }
+    public BookBorrowedException(string message) : base(message) { }
+}
+
+[Serializable]
+public class RoomBookedException : Exception
+{
+    public RoomBookedException() : base("Room is currently booked") { }
+    public RoomBookedException(string message) : base(message) { }
+}
+
+
+[Serializable]
+public class RoomNotFoundException : Exception
+{
+    public RoomNotFoundException() { }
+    public RoomNotFoundException(string message) : base(message) { }
+}
+
+
+[Serializable]
+public class LoginException : Exception
+{
+    public LoginException() { }
+    public LoginException(string message) : base(message) { }
+}
+
+
+[Serializable]
+public class InvalidEmailFormatException : Exception
+{
+    public InvalidEmailFormatException() : base("Invalid Email") { }
+    public InvalidEmailFormatException(string message) : base(message) { }
+
+}
+[Serializable]
+public class InvalidPasswordFormatException : Exception
+{
+    public InvalidPasswordFormatException() : base("Invalid Password") { }
+    public InvalidPasswordFormatException(string message) : base(message) { }
+
+}
+[Serializable]
+public class InvalidUsernameFormatException : Exception
+{
+    public InvalidUsernameFormatException() : base("Invalid Username") { }
+    public InvalidUsernameFormatException(string message) : base(message) { }
+
+}
